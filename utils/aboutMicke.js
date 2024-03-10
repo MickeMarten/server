11b56c85434074d4551af6659f6a5f16b92bb505
MickeMarten <Micke.Marten123@gmail.com>
2024-03-10 12:30:28 +0100

    lol

--- utils/aboutMicke.js
+++ utils/aboutMicke.js
@@ -2,28 +2,28 @@ export function aboutMicke(){
 
     const about = [
         {
-            picture: 'http://localhost:1337/api/images/AylinIda.JPG',
+            picture: 'https://shark-app-yja4d.ondigitalocean.app/api/images/AylinIda.JPG',
             description: 'Aylin and Ida creating stuff',
             year:'2019',
             includes: ['Ida', 'Aylin']
         },
 
         {
-             picture: 'http://localhost:1337/api/images/Bosna.jpeg',
+             picture: 'https://shark-app-yja4d.ondigitalocean.app/api/images/Bosna.jpeg',
             description: 'Micke living his dream',
             year:'2022',
             includes: 'Micke'
 
         },
           {
-             picture: 'http://localhost:1337/api/images/MickeAylin.jpeg',
+             picture: 'https://shark-app-yja4d.ondigitalocean.app/api/images/MickeAylin.jpeg',
             description: 'Micke and Aylin having a challenge',
             year:'2023',
             includes: ['Micke', 'Aylin']
 
         },
          {
-             picture: 'http://localhost:1337/api/images/Sara.jpg',
+             picture: 'https://shark-app-yja4d.ondigitalocean.app/api/images/Sara.jpg',
             description: 'Sara on the playground',
             year:'2024',
             includes: 'Sara'
